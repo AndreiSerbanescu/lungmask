4489355d1be2c93d699c6dd0dbfe6d0441f28d20
Andrei Serbanescu <andrei.serbanescu16@imperial.ac.uk>
2020-06-22 12:54:29 +0300

    cleanup

--- files/interface/test/test.py
+++ files/interface/test/test.py
@@ -2,7 +2,6 @@ import subprocess as sb
 
 if __name__ == "__main__":
 
-    # TODO rename to listen.py
     print("Testing listen.py")
     listen_cmd = "pytest /app/test/test_listen.py"
     sb.call([listen_cmd], shell=True)
